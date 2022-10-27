ef1969b28e58d77f47ac8df7e08be41d1621d078
Yrahcaz7 <zachary.shive@gmail.com>
2022-10-27 16:31:43 -0400

    behind-the scenes adjustments & fixes

--- js/technical/displays.js
+++ js/technical/displays.js
@@ -1,23 +1,16 @@
 function prestigeButtonText(layer) {
-	if (layers[layer].prestigeButtonText !== undefined)
-		return run(layers[layer].prestigeButtonText(), layers[layer]);
-	if (tmp[layer].type == "normal")
-		return `${player[layer].points.lt(1e3) ? (tmp[layer].resetDescription !== undefined ? tmp[layer].resetDescription : "Reset for ") : ""}+<b>${formatWhole(tmp[layer].resetGain)}</b> ${tmp[layer].resource} ${tmp[layer].resetGain.lt(100) && player[layer].points.lt(1e3) ? `<br><br>Next at ${(tmp[layer].roundUpCost ? formatWhole(tmp[layer].nextAt) : format(tmp[layer].nextAt))} ${tmp[layer].baseResource}` : ""}`
-	if (tmp[layer].type == "static")
-		return `${tmp[layer].resetDescription !== undefined ? tmp[layer].resetDescription : "Reset for "}+<b>${formatWhole(tmp[layer].resetGain)}</b> ${tmp[layer].resource}<br><br>${player[layer].points.lt(30) ? (tmp[layer].baseAmount.gte(tmp[layer].nextAt) && (tmp[layer].canBuyMax !== undefined) && tmp[layer].canBuyMax ? "Next:" : "Req:") : ""} ${formatWhole(tmp[layer].baseAmount)} / ${(tmp[layer].roundUpCost ? formatWhole(tmp[layer].nextAtDisp) : format(tmp[layer].nextAtDisp))} ${tmp[layer].baseResource}`
-	if (tmp[layer].type == "none")
-		return "";
+	if (layers[layer].prestigeButtonText !== undefined) return run(layers[layer].prestigeButtonText(), layers[layer]);
+	if (tmp[layer].type == "normal") return `${player[layer].points.lt(1e3) ? (tmp[layer].resetDescription !== undefined ? tmp[layer].resetDescription : "Reset for ") : ""}+<b>${formatWhole(tmp[layer].resetGain)}</b> ${tmp[layer].resource} ${tmp[layer].resetGain.lt(100) && player[layer].points.lt(1e3) ? `<br><br>Next at ${(tmp[layer].roundUpCost ? formatWhole(tmp[layer].nextAt) : format(tmp[layer].nextAt))} ${tmp[layer].baseResource}` : ""}`
+	if (tmp[layer].type == "static") return `${tmp[layer].resetDescription !== undefined ? tmp[layer].resetDescription : "Reset for "}+<b>${formatWhole(tmp[layer].resetGain)}</b> ${tmp[layer].resource}<br><br>${player[layer].points.lt(30) ? (tmp[layer].baseAmount.gte(tmp[layer].nextAt) && (tmp[layer].canBuyMax !== undefined) && tmp[layer].canBuyMax ? "Next:" : "Req:") : ""} ${formatWhole(tmp[layer].baseAmount)} / ${(tmp[layer].roundUpCost ? formatWhole(tmp[layer].nextAtDisp) : format(tmp[layer].nextAtDisp))} ${tmp[layer].baseResource}`
+	if (tmp[layer].type == "none") return "";
 	return "You need prestige button text";
 };
 
 function constructNodeStyle(layer) {
 	let style = [];
-	if ((tmp[layer].isLayer && layerunlocked(layer)) || (!tmp[layer].isLayer && tmp[layer].canClick));
-		style.push({'background-color': tmp[layer].color});
-	if (tmp[layer].image !== undefined)
-		style.push({'background-image': 'url("' + tmp[layer].image + '")'});
-	if(tmp[layer].notify && player[layer].unlocked)
-		style.push({'box-shadow': 'var(--hqProperty2a), 0 0 20px ' + tmp[layer].trueGlowColor});
+	if ((tmp[layer].isLayer && layerunlocked(layer)) || (!tmp[layer].isLayer && tmp[layer].canClick)) style.push({'background-color': tmp[layer].color});
+	if (tmp[layer].image !== undefined) style.push({'background-image': 'url("' + tmp[layer].image + '")'});
+	if (tmp[layer].notify && player[layer].unlocked) style.push({'box-shadow': 'var(--hqProperty2a), 0 0 20px ' + tmp[layer].trueGlowColor});
 	style.push(tmp[layer].nodeStyle);
 	return style;
 };
@@ -29,7 +22,7 @@ function challengeStyle(layer, id) {
 };
 
 function challengeButtonText(layer, id) {
-	let text = ["Finish", "Exit Early", "Completed", "Start"]
+	let text = ["Finish", "Exit Early", "Completed", "Start"];
 	if (tmp[layer].challenges[id].buttonText) {
 		if (typeof tmp[layer].challenges[id].buttonText == "function" && typeof tmp[layer].challenges[id].buttonText() == "object") {
 			if (tmp[layer].challenges[id].buttonText()[0]) text[0] = tmp[layer].challenges[id].buttonText()[0];
@@ -49,9 +42,7 @@ function challengeButtonText(layer, id) {
 function achievementStyle(layer, id) {
 	let ach = tmp[layer].achievements[id];
 	let style = [];
-	if (ach.image){ 
-		style.push({'background-image': 'url("' + ach.image + '")'})
-	};
+	if (ach.image) style.push({'background-image': 'url("' + ach.image + '")'});
 	if (!ach.unlocked) style.push({'visibility': 'hidden'});
 	style.push(ach.style);
 	return style;
@@ -93,20 +84,18 @@ function updateOomps(diff) {
 function constructBarStyle(layer, id) {
 	let bar = tmp[layer].bars[id];
 	let style = {};
-	if (bar.progress instanceof Decimal)
-		bar.progress = bar.progress.toNumber();
-	bar.progress = (1 -Math.min(Math.max(bar.progress, 0), 1)) * 100;
-	style.dims = {'width': bar.width + "px", 'height': bar.height + "px"}
-	let dir = bar.direction;
-	style.fillDims = {'width': (bar.width + 0.5) + "px", 'height': (bar.height + 0.5)  + "px"}
-	switch(bar.direction) {
+	if (bar.progress instanceof Decimal) bar.progress = bar.progress.toNumber();
+	bar.progress = (1 - Math.min(Math.max(bar.progress, 0), 1)) * 100;
+	style.dims = {'width': bar.width + "px", 'height': bar.height + "px"};
+	style.fillDims = {'width': (bar.width + 0.5) + "px", 'height': (bar.height + 0.5) + "px"};
+	switch (bar.direction) {
 		case UP:
 			style.fillDims['clip-path'] = 'inset(' + bar.progress + '% 0% 0% 0%)';
-			style.fillDims.width = bar.width + 1 + 'px';
+			style.fillDims.width = (bar.width + 1) + 'px';
 			break;
 		case DOWN:
 			style.fillDims['clip-path'] = 'inset(0% 0% ' + bar.progress + '% 0%)';
-			style.fillDims.width = bar.width + 1 + 'px';
+			style.fillDims.width = (bar.width + 1) + 'px';
 			break;
 		case RIGHT:
 			style.fillDims['clip-path'] = 'inset(0% ' + bar.progress + '% 0% 0%)';
@@ -117,15 +106,13 @@ function constructBarStyle(layer, id) {
 		case DEFAULT:
 			style.fillDims['clip-path'] = 'inset(0% 50% 0% 0%)';
 	};
-	if (bar.instant) {
-		style.fillDims['transition-duration'] = '0s';
-	};
+	if (bar.instant) style.fillDims['transition-duration'] = '0s';
 	return style;
 };
 
 function constructTabFormat(layer, id, family) {
-	let tabTemp, tabLayer, tabFunc, location, key
-	if (id === undefined){
+	let tabTemp, tabLayer, tabFunc, location, key;
+	if (id === undefined) {
 		tabTemp = tmp[layer].tabFormat;
 		tabLayer = layers[layer].tabFormat;
 		tabFunc = funcs[layer].tabFormat;
@@ -143,11 +130,8 @@ function constructTabFormat(layer, id, family) {
 		tabFunc = funcs[layer].microtabs[family][id].content;
 		location = tmp[layer].microtabs[family][id];
 		key = "tabFormat";
-
 	};
-	if (isFunction(tabLayer)) {
-		return tabLayer.bind(location)();
-	};
+	if (isFunction(tabLayer)) return tabLayer.bind(location)();
 	updateTempData(tabLayer, tabTemp, tabFunc, {layer, id, family});
 	return tabTemp;
 };
@@ -161,27 +145,24 @@ function updateTabFormat(layer) {
 	if (layers[layer]?.tabFormat === undefined) return;
 	let tab = player.subtabs[layer]?.mainTabs
 	if (isFunction(layers[layer].tabFormat)) {
-		Vue.set(temp[layer], 'tabFormat', layers[layer].tabFormat())
+		Vue.set(temp[layer], 'tabFormat', layers[layer].tabFormat());
 	} else if (Array.isArray(layers[layer].tabFormat)) {
-		Vue.set(temp[layer], 'tabFormat', constructTabFormat(layer))
+		Vue.set(temp[layer], 'tabFormat', constructTabFormat(layer));
 	} else if (isPlainObject(layers[layer].tabFormat)) {
-		if (layers[layer].tabFormat[tab].embedLayer === undefined)
-		Vue.set(temp[layer].tabFormat[tab], 'content', constructTabFormat(layer, tab))
+		if (layers[layer].tabFormat[tab].embedLayer === undefined) Vue.set(temp[layer].tabFormat[tab], 'content', constructTabFormat(layer, tab));
 	};
 
 	// Check for embedded layer
 	if (isPlainObject(tmp[layer].tabFormat) && tmp[layer].tabFormat[tab].embedLayer !== undefined) { 
-		updateTabFormat(tmp[layer].tabFormat[tab].embedLayer)
+		updateTabFormat(tmp[layer].tabFormat[tab].embedLayer);
 	};
 
 	// Update microtabs
 	for (family in layers[layer].microtabs) {
 		tab = player.subtabs[layer][family];
 		if (tmp[layer].microtabs[family][tab]) {
-			if (tmp[layer].microtabs[family][tab].embedLayer)
-				updateTabFormat(tmp[layer].microtabs[family][tab].embedLayer);
-			else
-				Vue.set(temp[layer].microtabs[family][tab], 'content', constructTabFormat(layer, tab, family));
+			if (tmp[layer].microtabs[family][tab].embedLayer) updateTabFormat(tmp[layer].microtabs[family][tab].embedLayer);
+			else Vue.set(temp[layer].microtabs[family][tab], 'content', constructTabFormat(layer, tab, family));
 		};
 	};
 };
