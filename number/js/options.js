2de3577842c385a81604d6ba204822342107b555
Yrahcaz7 <zachary.shive@gmail.com>
2025-02-13 15:53:15 -0500

    fix & join shared options

--- number/js/options.js
+++ number/js/options.js
@@ -12,7 +12,7 @@ function getStartOptions() {
 		forceOneTab: false,
 		forceTooltips: true,
 		hideMilestonePopups: false,
-		extendplaces: false,
+		extendPlaces: false,
 	});
 };
 
@@ -40,7 +40,7 @@ const optionGrid = [
 ];
 
 function formatOpt(opt) {
-	if (opt) return 'ON'
+	if (opt) return 'ON';
 	return 'OFF';
 };
 
@@ -50,44 +50,4 @@ function toggleOpt(name) {
 	// special
 	if (name == 'hqTree') changeTreeQuality();
 	else if (name == 'forceOneTab') needsCanvasUpdate = true;
-};
-
-function changeTreeQuality() {
-	let on = options.hqTree;
-	document.body.style.setProperty('--hqProperty1', on ? '2px solid' : '4px solid');
-	document.body.style.setProperty('--hqProperty2a', on ? '-4px -4px 4px #00000040 inset' : '-4px -4px 4px #00000000 inset');
-	document.body.style.setProperty('--hqProperty2b', on ? '0px 0px 20px var(--background)' : '');
-	document.body.style.setProperty('--hqProperty3', on ? '2px 2px 4px #00000040' : 'none');
-};
-
-function toggleAuto(toggle) {
-	player[toggle[0]][[toggle[1]]] = !player[toggle[0]][toggle[1]];
-	needCanvasUpdate = true;
-};
-
-const MS_DISPLAYS = ['ALL', 'LAST, AUTO, INCOMPLETE (recommended)', 'AUTOMATION, INCOMPLETE (recommended)', 'INCOMPLETE', 'NONE'];
-
-const MS_SETTINGS = ['always', 'last', 'automation', 'incomplete', 'never'];
-
-function adjustMSDisp() {
-	options.msDisplay = MS_SETTINGS[(MS_SETTINGS.indexOf(options.msDisplay) + 1) % MS_SETTINGS.length];
-};
-
-function milestoneShown(layer, id) {
-	if (layers[layer].milestones[id] === undefined) return false;
-	let complete = player[layer].milestones.includes(id);
-	let auto = layers[layer].milestones[id].toggles;
-	switch (options.msDisplay) {
-		case 'always':
-			return true;
-		case 'last':
-			return auto || !complete || player[layer].lastMilestone === id;
-		case 'automation':
-			return auto || !complete;
-		case 'incomplete':
-			return !complete;
-		case 'never':
-			return false;
-	};
-	return false;
 };
--- primordial/js/options.js
+++ primordial/js/options.js
@@ -25,12 +25,12 @@ const optionGrid = [
 		{text: "Save game", onClick: save},
 		{opt: "autosave", text() {return "Autosave: " + formatOpt(options[this.opt])}, onClick: toggleOpt},
 		{text: "HARD RESET", onClick: hardReset},
-		{opt: "colorDisplayMode", text() {return "Color Text Mode: " + DISPLAY_MODES[options[this.opt]]}, onClick: displayMode},
+		{opt: "colorDisplayMode", text() {return "Color Text Mode: " + DISPLAY_MODES[options[this.opt]]}, onClick: adjustColorDispMode},
 	], [
 		{text: "Export save to clipboard", onClick: exportSave},
 		{text: "Import save", onClick: importSave},
 		{opt: "offlineProd", text() {return "Offline Prod: " + formatOpt(options[this.opt])}, onClick: toggleOpt},
-		{opt: "colorDisplay", text() {return "Colored Text: " + COLOR_DISPLAYS[options[this.opt]]}, onClick: colorDisplay},
+		{opt: "colorDisplay", text() {return "Colored Text: " + COLOR_DISPLAYS[options[this.opt]]}, onClick: adjustColorDisp},
 	], [
 		{opt: "theme", text() {return "Theme: " + getThemeName()}, onClick: switchTheme},
 		{opt: "msDisplay", text() {return "Show Milestones: " + MS_DISPLAYS[MS_SETTINGS.indexOf(options[this.opt])]}, onClick: adjustMSDisp},
@@ -60,59 +60,19 @@ function toggleOpt(name) {
 	else if (name == 'forceOneTab') needsCanvasUpdate = true;
 };
 
-function changeTreeQuality() {
-	let on = options.hqTree;
-	document.body.style.setProperty('--hqProperty1', on ? '2px solid' : '4px solid');
-	document.body.style.setProperty('--hqProperty2a', on ? '-4px -4px 4px #00000040 inset' : '-4px -4px 4px #00000000 inset');
-	document.body.style.setProperty('--hqProperty2b', on ? '0px 0px 20px var(--background)' : '');
-	document.body.style.setProperty('--hqProperty3', on ? '2px 2px 4px #00000040' : 'none');
-};
-
-function toggleAuto(toggle) {
-	player[toggle[0]][[toggle[1]]] = !player[toggle[0]][toggle[1]];
-	needCanvasUpdate = true;
-};
-
-const MS_DISPLAYS = ['ALL', 'LAST, AUTO, INCOMPLETE (recommended)', 'AUTOMATION, INCOMPLETE (recommended)', 'INCOMPLETE', 'NONE'];
-
-const MS_SETTINGS = ['always', 'last', 'automation', 'incomplete', 'never'];
-
-function adjustMSDisp() {
-	options.msDisplay = MS_SETTINGS[(MS_SETTINGS.indexOf(options.msDisplay) + 1) % MS_SETTINGS.length];
-};
-
-function milestoneShown(layer, id) {
-	if (layers[layer].milestones[id] === undefined) return false;
-	let complete = player[layer].milestones.includes(id);
-	let auto = layers[layer].milestones[id].toggles;
-	switch (options.msDisplay) {
-		case 'always':
-			return true;
-		case 'last':
-			return auto || !complete || player[layer].lastMilestone === id;
-		case 'automation':
-			return auto || !complete;
-		case 'incomplete':
-			return !complete;
-		case 'never':
-			return false;
-	};
-	return false;
-};
-
 const DISPLAY_MODES = ['ALL (recommended)', 'ONLY SPECIAL', 'SPECIAL AND TITLES', 'SPECIAL AND REFRENCES'];
 
 const COLOR_DISPLAYS = ['ON - NORMAL (recommended)', 'ON - ALWAYS DARK', 'OFF (recommended for colorblind)'];
 
 let colorValue = [[true, true], 'normal'];
 
-function displayMode() {
+function adjustColorDispMode() {
 	options.colorDisplayMode++;
 	if (options.colorDisplayMode >= DISPLAY_MODES.length) options.colorDisplayMode = 0;
 	calculateColorValue();
 };
 
-function colorDisplay() {
+function adjustColorDisp() {
 	options.colorDisplay++;
 	if (options.colorDisplay >= COLOR_DISPLAYS.length) options.colorDisplay = 0;
 	calculateColorValue();
