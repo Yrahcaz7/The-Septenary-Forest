c5bb5b3a7ef62c89d9debf57027c6819ce0d2e68
Yrahcaz7 <zachary.shive@gmail.com>
2025-06-07 17:51:54 -0400

    realm: add color to faction coin stats

--- realm/js/layers.js
+++ realm/js/layers.js
@@ -651,7 +651,10 @@ addLayer("S", {
 				"blank",
 				["display-text", () => "<h3>CLICKS</h3><br>Your best coins/click is <b>" + format(player.stats[index].bestClickValue) + "</b><br>You have generated <b>" + format(player.stats[index].totalClickValue) + "</b> coins from clicking" + (index === 0 ? "" : "<br>Your best times clicked is <b>" + formatWhole(player.stats[index].bestClicks) + "</b>") + "<br>You have clicked <b>" + formatWhole(player.stats[index].totalClicks) + "</b> times", () => { return {color: lighten(getSideColor(0))} }],
 				"blank",
-				["display-text", () => "<h3>FACTION COINS</h3><br>Your best faction coins is <b>" + formatWhole(player.stats[index].FCbest) + "</b><br>You have found <b>" + formatWhole(player.stats[index].FCtotal) + "</b> faction coins<br>You have <b>" + format(player.stats[index].FCchance) + "%</b> best faction coin chance"],
+				["display-text", () => "<h3>FACTION COINS</h3><br>Your best faction coins is <b>" + formatWhole(player.stats[index].FCbest) + "</b><br>You have found <b>" + formatWhole(player.stats[index].FCtotal) + "</b> faction coins<br>You have <b>" + format(player.stats[index].FCchance) + "%</b> best faction coin chance", () => {
+					const faction = getAllianceIndex();
+					if (faction >= 0) return {color: lighten(factionColor[faction])};
+				}],
 				"blank",
 				["display-text", () => "<h3>CREATIONS</h3><br>Your best creations is <b>" + formatWhole(player.stats[index].creations) + "</b>", () => { return {color: lighten(layers.C.color)} }],
 				"blank",
