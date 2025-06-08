77127a4c00cfaab07fec472f73f6aff943512c42
Yrahcaz7 <zachary.shive@gmail.com>
2025-06-08 16:48:40 -0400

    simplify loader.js

--- shared/js/loader.js
+++ shared/js/loader.js
@@ -1,6 +1,5 @@
 for (const file in modInfo.modFiles) {
 	let script = document.createElement("script");
 	script.setAttribute("src", "js/" + modInfo.modFiles[file]);
-	script.setAttribute("async", "false");
-	document.head.insertBefore(script, document.getElementById("temp"));
+	document.head.appendChild(script);
 };
